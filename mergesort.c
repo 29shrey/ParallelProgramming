40e468d358b616afacf6b7fed3ac0fe07c9b38a6
29shrey <114428925+29shrey@users.noreply.github.com>
2023-12-18 20:41:27 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -6,10 +6,8 @@ void merge(int a[],int l,int mid,int h){
     int n1 = mid-l+1;
     int n2 = h-mid;
     int arr1[n1],arr2[n2];
-    for(int i=0;i<n1;i++) 
-        arr1[i] = a[l+i];
-    for(int i=0;i<n2;i++) 
-        arr2[i] = a[mid+1+i];
+    for(int i=0;i<n1;i++) arr1[i] = a[l+i];
+    for(int i=0;i<n2;i++) arr2[i] = a[mid+1+i];
     int i=0,j=0,k=l;
     while(i<n1 && j<n2){
         if(arr1[i]<=arr2[j]){
@@ -27,7 +25,6 @@ void merge(int a[],int l,int mid,int h){
     }
 }
 
-
 void mergesortParallel(int a[],int l,int h){
     if(l<h){
         int mid = l+(h-l)/2;
@@ -42,11 +39,9 @@ void mergesortParallel(int a[],int l,int h){
     }
 }
 
-
 void mergesortSerial(int a[],int l,int h){
     if(l<h){
-        int mid = l+(h-l)/2; 
-        {
+        int mid = l+(h-l)/2; {
             mergesortSerial(a,l,mid);
             mergesortSerial(a,mid+1,h);
         }
@@ -55,19 +50,24 @@ void mergesortSerial(int a[],int l,int h){
 }
 
 
-void main(){
+void main()
+{
     int *a,num,i;
     printf("enter number:");
     scanf("%d",&num);
     a = (int *)malloc(sizeof(int)*num);
+
     double start = omp_get_wtime();
     mergesortSerial(a,0,num-1);
     double end = omp_get_wtime();
+
     double val = end - start;
     printf("\nTime for serial is:%f\n",val);
+
     start = omp_get_wtime();
     mergesortParallel(a,0,num-1);
     end = omp_get_wtime();
+
     val = end-start;
     printf("Time for parallel execution is %f\n",val);
 }
