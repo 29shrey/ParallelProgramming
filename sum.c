0b39d4b7ae8058236c6f29e77347809d4b0d01ef
29shrey <114428925+29shrey@users.noreply.github.com>
2023-12-18 20:39:11 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -5,10 +5,12 @@
 void main(){
     int n =100;
     int sum = 0; // global so should use critical while updating
+
     #pragma omp parallel for
     for(int i=1;i<=n;i++){
-        #pragma omp critical
-        sum+=i;
+    #pragma omp critical
+    sum+=i;
     }
+
     printf("sum is %d\n",sum);
 }
